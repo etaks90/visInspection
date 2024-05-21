6015c48e982eb62dea81ebbbe71fa2a4163743ea
dataAiOliver <data.ai.oliver@gmail.com>
2024-05-21 18:50:54 +0200

    connections

--- app.py
+++ app.py
@@ -1,39 +1,60 @@
 import streamlit as st
-from PIL import Image
-import io
+import pandas as pd
+import numpy as np
 
-def process_image(image_data):
-    """ A function to process the image. For example, print its dimensions. """
-    image = Image.open(image_data)
-    st.write("Image dimensions:", image.size)  # Prints dimensions
+def print_file_names(uploaded_files):
+    for uploaded_file in uploaded_files:
+        print(uploaded_file.name)
 
-    predicted_label = "def"
-    return predicted_label
+def generate_data():
+    # Simulate data generation for DataFrame
+    data = {'Column1': np.random.randint(0, 100, 4),
+            'Column2': np.random.randint(0, 100, 4)}
+    return pd.DataFrame(data)
+
+# Define your pages as functions
+def upload_images():
+    st.title('Multiple File Upload Demo')
+
+    # Create a file uploader to accept multiple files
+    uploaded_files = st.file_uploader("Choose files", accept_multiple_files=True)
+
+    if st.button('Print File Names'):
+        if uploaded_files:
+            print_file_names(uploaded_files)
+        else:
+            st.write("No files uploaded yet. Please upload some files and try again.")
+
+def dashboard():
+    st.header("Visualization of evaluated images.")
+    st.write("Welcome to the About Page! Learn more about what we do and our mission.")
+
+    # Initial DataFrame
+    df = generate_data()
+
+    # Button to refresh the DataFrame
+    if st.button('Refresh Data'):
+        df = generate_data()  # Generate new data for the DataFrame
+
+    # Display the DataFrame
+    st.write("Here is our DataFrame:")
+    st.dataframe(df)
+
+    # Display a line chart of the DataFrame
+    st.line_chart(df)
+    
 
 def main():
-    st.title("Image Upload Demo")
+    st.set_page_config(page_title="My Streamlit App", layout="wide", initial_sidebar_state="collapsed")
 
-    # Create a file uploader to upload images
-    uploaded_file = st.file_uploader("Drag and Drop or Click to Upload an Image", type=['png', 'jpg', 'jpeg'])
-
-    if uploaded_file is not None:
-        
-
-        # Display the uploaded image
-        image_data = io.BytesIO(uploaded_file.getvalue())
-        # Process the image (example function)
-        predicted_label = process_image(image_data)
-        image = Image.open(image_data)
-        # To See details
-        file_details = {"filename": uploaded_file.name, "filetype": uploaded_file.type,
-                        "filesize": uploaded_file.size,
-                        "true-label": uploaded_file.name.split("_")[1],
-                        "predicted-label": predicted_label
-                        }
-        st.write(file_details)
-        st.image(image, caption='Uploaded Image.', use_column_width=True)
-
-        
+    # Use tabs for navigation
+    tab1, tab2 = st.tabs(["Upload Images", "Dashboard"])
+    
+    with tab1:
+        upload_images()
+    
+    with tab2:
+        dashboard()
 
 if __name__ == "__main__":
     main()
--- function_app.py
+++ function_app.py
@@ -1,11 +1,58 @@
 import azure.functions as func
-import logging
+import logging, os, json, sys, codecs
+from lib.utils import *
+import pandas as pd
 
+##########################################################################################
+##########################PREPARE STUFF################################################
+###########################################################################################
+# Set default encoding to UTF-8
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+sys.stderr = codecs.getwriter("utf-8")(sys.stderr.detach())
+
+# PARAM
+connection_string = os.getenv("CSTR__BLOB__VISINSP")
+container_name = "image-uploads"
+log_schema = "VISUAL_INSPECTION"
+log_table = "LOG__VISUAL_INSPECTION"
+
+# DB CONNECTION
+eng = get_engine()
+
+# BLOB CONNECTION
+blob_service_client = get_blob_service_client(connection_string)
+
+# model
+loaded_model = get_keras_model(blob_service_client, "local", r"C:\Users\oliver.koehn\Documents\gitProjects\qualityInspection\examples\trained_model.keras")
+
+##########################################################################################
+##########################AZURE FUNCTION STUFF################################################
+##########################################################################################
+
+# FUNCTIONS
 app = func.FunctionApp()
 
 @app.blob_trigger(arg_name="myblob", path="image-uploads",
                                connection="connectionVisualInspection") 
 def trigger_image_evaluation(myblob: func.InputStream):
+    """
+    For some reason the variable 'myblob.name' starts with the container_name. Therefore
+    we remove this below.
+    """
+    fp__img__blob = "/".join(myblob.name.split("/")[1:])
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
+                f"fp__img__blob: {fp__img__blob}"
                 f"Blob Size: {myblob.length} bytes")
+    
+    
+    
+
+    # EVAL
+    prob__ok = eval_img_from_blob(loaded_model, blob_service_client, container_name, fp__img__blob)
+
+    # LOG TO DB
+    j__db = {"FN__BLOB" : fp__img__blob.split("/")[-1], "CONTAINER_NAME" : container_name, "PROBABILITY_OK": round(prob__ok, 4), "FP__BLOB" : fp__img__blob}
+    logging.info(j__db)
+    df = pd.DataFrame(j__db, index=[0])
+    df.to_sql(log_table, eng, schema=log_schema, if_exists="append", index=False)
